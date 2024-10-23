8825814f85d9291e658794b6e9893e6e02f0a7ed
Fabian Leutenegger <fabian.leutenegger@bluewin.ch>
2024-10-23 16:03:48 +0200

    fuxi: Allow gettid syscall for modemManager
    10-23 15:51:48.123  4030  4030 V Telephony: registerImsRegistrationCallback: registration success
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: failed to get path of fd 3: No such file or directory
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: allowing syscall: connect
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: allowing syscall: fcntl
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: allowing syscall: sendto
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: allowing syscall: socket
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: allowing syscall: writev
    10-23 15:51:48.139  9378  9378 W modemManager: libminijail[9378]: logging seccomp filter failures
    10-23 15:51:48.140  1350  1350 I QESDK_SESSION_MANAGER: Connected to Client
    10-23 15:51:48.140  1350  1350 I QESDK_SESSION_MANAGER: WAITING to get connected by client
    10-23 15:51:48.140  9378  9378 E modemManager: libminijail[9378]: blocked syscall: gettid
    10-23 15:51:48.140  1350  9381 I QESDK_SESSION_MANAGER: Manager waiting to receive
    10-23 15:51:48.141  1350  9381 E QESDK_SESSION_MANAGER: A service hal has crashed!
    10-23 15:51:48.155     1     1 I init    : Service 'vendor.modemManager' (pid 9378) received signal 31
    10-23 15:51:48.155     1     1 I init    : Sending signal 9 to service 'vendor.modemManager' (pid 9378) process group...
    10-23 15:51:48.155     1     1 I libprocessgroup: Removed cgroup /sys/fs/cgroup/uid_1001/pid_9378
    10-23 15:51:48.155     1     1 E init    : process with updatable components 'vendor.modemManager' exited 4 times in 4 minutes
    
    Change-Id: I3cb61234d8815f39362c3841cd60fed3e4cecfe8

--- extract-files.sh
+++ extract-files.sh
@@ -90,7 +90,7 @@ function blob_fixup() {
             [ "$2" = "" ] && return 0
             sed -i '/persist.vendor.radio.redir_party_num/ s/true/false/g' "${2}"
             ;;
-        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
+        vendor/etc/seccomp_policy/atfwd@2.0.policy | vendor/etc/seccomp_policy/modemManager.policy | vendor/etc/seccomp_policy/wfdhdcphalservice.policy)
             [ -n "$(tail -c 1 "${2}")" ] && echo >> "${2}"
             grep -q "gettid: 1" "${2}" || echo "gettid: 1" >> "${2}"
             ;;
